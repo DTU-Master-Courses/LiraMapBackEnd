7ba6599a379bd87dc4d1e1f7de0312ec4715f9f3
Tswagerman <thomasswagerman@hotmail.com>
2022-10-02 12:14:34 +0200

    implemented router

--- lira_backend_api/lira_backend_api/__main__.py
+++ lira_backend_api/lira_backend_api/__main__.py
@@ -9,8 +9,11 @@ from lira_backend_api.database.db import get_db
 from lira_backend_api.v1.endpoints.crud import get_measurementtype,get_measurementmodel
 from lira_backend_api.settings import settings
 
+from lira_backend_api.v1.routers import measurements
+
 
 app = FastAPI()
+app.include_router(measurements.router)
 # TODO: We need to change the origins of this for production
 app.add_middleware(
     CORSMiddleware,
@@ -19,19 +22,6 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-
-@app.get("/type/{measurement_type_id}", response_model=MeasurementTypes)
-def root(measurement_type_id: str, db: Session = Depends(get_db)):
-    result = get_measurementtype(measurement_type_id, db)
-
-    return result
-
-@app.get("/model/{measurement_model_id}", response_model=MeasurementModel)
-def root(measurement_model_id: str, db: Session = Depends(get_db)):
-    result = get_measurementmodel(measurement_model_id, db)
-
-    return result
 
 
 if __name__ == "__main__":
---
+++ lira_backend_api/lira_backend_api/v1/routers/measurements.py
@@ -0,0 +1,20 @@
+from fastapi import APIRouter, status, Response, Depends
+from sqlalchemy.orm import Session
+
+from lira_backend_api.core.schemas import MeasurementTypes, MeasurementModel
+from lira_backend_api.v1.endpoints.crud import get_measurementtype, get_measurementmodel
+from lira_backend_api.database.db import get_db
+
+router = APIRouter(prefix='/measurement')
+
+@router.get("/Type/{measurement_type_id}", response_model=MeasurementTypes)
+def get_measurement_type(measurement_type_id: str, db: Session = Depends(get_db)):
+    result = get_measurementtype(measurement_type_id, db)
+
+    return result
+
+@router.get("/Model/{measurement_model_id}", response_model=MeasurementModel)
+def get_measurement_model(measurement_model_id: str, db: Session = Depends(get_db)):
+    result = get_measurementmodel(measurement_model_id, db)
+
+    return result
