43be9ca670f0728cc5094f0753016454c3e7abeb
RAN <wangrandk@users.noreply.github.com>
2022-10-14 01:31:31 +0200

    Update test_device.py
    test_device done

--- lira_backend_api/lira_backend_api/v1/routers/test_device.py
+++ lira_backend_api/lira_backend_api/v1/routers/test_device.py
@@ -5,7 +5,7 @@
 # from lira_backend_api.v1.routers.utils import get_deviceid
 # from lira_backend_api.database.db import get_db
 from fastapi.testclient import TestClient
-from .device import router
+
 import json
 from lira_backend_api.main import app
 # router = APIRouter(prefix="/device")
@@ -18,19 +18,22 @@ client = TestClient(app)
 # testing sample from device table row No.4
 # "DeviceId"	"Created_Date"	"Updated_Date"	"FK_SourceType"
 # "47690661-f343-4591-920c-50dcd59f74e2"	"2020-05-28 00:00:00+00"	"2020-05-28 00:00:00+00"	"fb64715d-09d1-4fd9-8912-685364c7d446"
+# 0cb38d9f-3601-4179-b5ef-403a999c9021
 def test_get_device_id():
-    res = client.get("/device/0cb38d9f-3601-4179-b5ef-403a999c9021")
-    res_body= json.loads(res.text)
+    res = client.get("/device/47690661-f343-4591-920c-50dcd59f74e2")
     assert res.status_code == 200
-    # assert res_body.get("id")== "47690661-f343-4591-920c-50dcd59f74e2"
-    # assert res.json() == {
-    #     "id": "47690661-f343-4591-920c-50dcd59f74e2",
-    #     "created_date": "2020-05-28 00:00:00+00",
-    #     "updated_date": "2020-05-28 00:00:00+00",
-    #     "fk_sourcetype": "fb64715d-09d1-4fd9-8912-685364c7d446"
-    # }
-
-# def test_get_inexistent_device_id():
-#     res = client.get("/47690661-")
-#     assert res.status_code == 404
-#     assert res.json() == {"detail": "Device not found"}
+    print(res.status_code)
+    print  (res.json())
+    assert res.json() == {
+        'id': '47690661-f343-4591-920c-50dcd59f74e2', 
+        'created_date': '2020-05-28T00:00:00+00:00', 
+        'updated_date': '2020-05-28T00:00:00+00:00', 
+        'fk_sourcetype': 'fb64715d-09d1-4fd9-8912-685364c7d446'
+        }
+   
+# given a false id, expecting a 404 error.
+def test_get_inexistent_device_id():
+    res = client.get("/47690661-")
+    assert res.status_code == 404
+    print  (res.json())
+    assert res.json() == {'detail': 'Not Found'}
